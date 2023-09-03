a3b33e0b85491d8bea9fe6d3afa169fd42a8bfd1
Thiago <thiagosaopedroo07@gmail.comm>
2023-09-03 19:37:22 -0300

    a

---
+++ teste6.py
@@ -0,0 +1,9 @@
+import random
+num = random.randint(1, 100000)
+print(num)
+
+from math import sqrt
+num = int(input('Digite um numero'))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+
