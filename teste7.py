9199d7b11e02c072aa7775d631313b972ea456ad
Thiago <thiagosaopedroo07@gmail.comm>
2023-09-05 18:41:17 -0300

    aula 9

---
+++ teste7.py
@@ -0,0 +1,22 @@
+frase = '    Curso em Vídeo Python   '
+print(frase[3])
+print(frase[3:13])
+print(frase[:13])
+print(frase.count('o')) #lê os o's minusculos
+print(frase.upper().count('O'))  #coloca em maiusculo e lê os O's maiusculos
+print(len(frase))   #tamanho da frase
+print(len(frase.strip())) #remove os espaços inuteis
+frase = print(frase.replace('Python', 'Android')) #frase = para atribuir o valor trocado
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.split()) #separa cada palavra em uma lista diferente
+dividido = frase.split
+print(dividido[2][3]) #Segunda palavra letra 3
+
+
+
+
+print ("""teasljnasjkdnaskndas
+dasdjjhasiudnakjsdnajk
+asjdhasdnajksdka
+askdhauisdjasdmaslkmd""")
