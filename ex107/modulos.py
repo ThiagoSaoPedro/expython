a06a5f1f02009ceddc85ef03f1eedf9a88bbcf38
Thiago <thiagosaopedroo07@gmail.comm>
2023-12-08 14:34:46 -0300

    ex107

---
+++ ex107/modulos.py
@@ -0,0 +1,18 @@
+def aumentar(preco):
+    preco = preco + (preco * 0.1)
+    print(f'O preço aumentado em 10% é: {preco}')
+
+
+def diminuir(preco):
+    preco = preco * 0.9
+    print(f'O preço diminuido em 10% é: {preco}')
+
+
+def metade(preco):
+    preco = preco / 2
+    print(f'A metade do preço é {preco}')
+
+
+def dobro(preco):
+    preco = preco * 2
+    print(f'O dobro do preco é {preco}')
---
+++ ex107/teste.py
@@ -0,0 +1,6 @@
+import modulos
+preco = 100
+modulos.aumentar(preco)
+modulos.diminuir(preco)
+modulos.dobro(preco)
+modulos.metade(preco)
